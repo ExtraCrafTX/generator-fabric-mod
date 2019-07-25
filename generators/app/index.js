82d516152e3ac457a6375227598c101917a420e5
Kaamil Jasani <kaamilj@yahoo.com>
2019-07-26 00:49:28 +0100

    Mod version prompt with SemVer checking

--- generators/app/index.js
+++ generators/app/index.js
@@ -41,6 +41,19 @@ module.exports = class extends Generator {
             return chalk.redBright("The description may not be blank");
           return true;
         }
+      },
+      {
+        type: 'input',
+        name: 'mod_version',
+        message: 'Mod version:',
+        default: '0.1.0',
+        validate: async (input, hash) => {
+          let semVerRegex = /^(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-(?:0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*)(?:\.(?:0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*))*)?(?:\+[\dA-Za-z-]+(?:\.[\dA-Za-z-]+)*)?$/;
+          if (!semVerRegex.test(input)) {
+            return chalk.redBright("Please format your version according to SemVer");
+          }
+          return true;
+        }
       }
     ];
 
