379ec98ef8a9d0f07650a5b47a7d9a4d9ce3a4db
Kaamil Jasani <kaamilj@yahoo.com>
2019-07-28 23:55:13 +0100

    fixed gitignore issue

--- generators/app/index.js
+++ generators/app/index.js
@@ -411,7 +411,7 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('.gitignore'),
+      this.templatePath('gitignore'),
       this.destinationPath('.gitignore')
     );
     this.fs.copy(
